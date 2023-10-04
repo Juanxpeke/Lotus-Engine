52e0c65bbfc797566b8138922b053c22ec1c7b8e
Juanxpeke <juanintendo.f11@gmail.com>
2023-10-03 22:37:26 -0300

    fix: fixed submodules errors on merge

--- renderer/path_manager.h
+++ renderer/path_manager.h
@@ -10,7 +10,7 @@ Placeholder script, don't modify it
 #include <string>
 #include <filesystem>
 
-static const std::string directoryPath = "C:/Users/juani/Desktop/Computer Graphics/Graphics Computing/OpenGL-Rendering-Engine/";
+static const std::string directoryPath = "C:/Users/juani/Desktop/Computer Graphics/Bachelor Thesis/OpenGL-Rendering-Engine/";
 
 static std::filesystem::path assetPath(const std::string& assetFilename) {
   return directoryPath + "assets/" + assetFilename;
