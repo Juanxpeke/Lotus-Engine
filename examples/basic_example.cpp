ec963059d28a8da6e9c37be6cb888de2a8e72b39
Juanxpeke <juanintendo.f11@gmail.com>
2024-04-23 19:19:33 -0400

    feat: updated traditional flat shader

--- examples/basic_example.cpp
+++ examples/basic_example.cpp
@@ -17,35 +17,64 @@ int width = 720;
 int height = 720;
 char title[256];
 
+const float cameraSpeed = 14.4f;
+const float cameraAngularSpeed = 2.0f;
+
+const int objectCount = 5000;
+
 void updateFromInputs(GLFWwindow* window, float dt, Camera* cameraPtr)
 {
+  // Translation
   if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
-    cameraPtr->translate(cameraPtr->getFrontVector() * dt * 8.f);
+    cameraPtr->translate(cameraPtr->getFrontVector() * dt * cameraSpeed);
   }
   if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
-    cameraPtr->translate(cameraPtr->getRightVector() * dt * -8.f);
+    cameraPtr->translate(cameraPtr->getRightVector() * dt * -cameraSpeed);
   }
   if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
-    cameraPtr->translate(cameraPtr->getFrontVector() * dt * -8.f);
+    cameraPtr->translate(cameraPtr->getFrontVector() * dt * -cameraSpeed);
   }
   if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
-    cameraPtr->translate(cameraPtr->getRightVector() * dt * 8.f);
+    cameraPtr->translate(cameraPtr->getRightVector() * dt * cameraSpeed);
 	}
   if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
-    cameraPtr->translate(glm::vec3(0.0f, 1.0f, 0.0f) * dt * 8.f);
+    cameraPtr->translate(glm::vec3(0.0f, 1.0f, 0.0f) * dt * cameraSpeed);
   }
   if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) {
-    cameraPtr->translate(glm::vec3(0.0f, 1.0f, 0.0f) * dt * -8.f);
+    cameraPtr->translate(glm::vec3(0.0f, 1.0f, 0.0f) * dt * -cameraSpeed);
   }
+  // Rotation
+  if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+    cameraPtr->rotate(cameraPtr->getRightVector(), dt * cameraAngularSpeed);
+  }
+  if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
+    cameraPtr->rotate(glm::vec3(0.0f, 1.0f, 0.0f), dt * cameraAngularSpeed);
+  }
+  if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+    cameraPtr->rotate(cameraPtr->getRightVector(), dt * -cameraAngularSpeed);
+  }
+  if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
+    cameraPtr->rotate(glm::vec3(0.0f, 1.0f, 0.0f), dt * -cameraAngularSpeed);
+  }
+  // Misc
   if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
     glfwSetWindowShouldClose(window, true);
   }
 }
 
+void createDirectionalLight(Renderer& renderer)
+{
+  DirectionalLight* directionalLight = renderer.createDirectionalLight();
+
+  directionalLight->rotate(glm::vec3(0.0f, 1.0f, 0.0f), glm::pi<float>() * 0.5f);
+  directionalLight->rotate(glm::vec3(0.0f, 0.0f, 1.0f), glm::pi<float>() * 0.25f);
+  directionalLight->setLightColor(glm::vec3(0.1f, 0.04f, 0.0f));
+}
+
 void createVent(Renderer& renderer)
 {
 	auto& meshManager = MeshManager::getInstance();
-	std::shared_ptr<Mesh> ventMesh = meshManager.loadMesh(assetPath("models/air_conditioner/AirConditioner.obj").string());
+	std::shared_ptr<Mesh> ventMesh = meshManager.loadMesh(assetPath("models/air_conditioner/AirConditioner.obj"));
 
 	std::shared_ptr<DiffuseFlatMaterial> ventMaterial = std::static_pointer_cast<DiffuseFlatMaterial>(renderer.createMaterial(MaterialType::DiffuseFlat));
 
@@ -90,14 +119,15 @@ int main()
   Renderer renderer;
 	renderer.startUp();
 
-	const int objectCount = 5000; 
+	renderer.setAmbientLight(glm::vec3(0.1, 0.1, 0.1));
+	createDirectionalLight(renderer);
 
 	for (int i = 0; i < objectCount; i++)
 	{
 		createVent(renderer);
 	}
 	
-	float lastTime = -1.0f;
+	float lastTime = glfwGetTime();
 
 	// Main while loop
 	while (!glfwWindowShouldClose(window))
@@ -105,15 +135,13 @@ int main()
 		glfwPollEvents();
 		
 		float currentTime = glfwGetTime();
-
-		if (lastTime >= 0.0f) {
-			float dt = currentTime - lastTime;
-			std::cout << "FPS: " << 1.0f / dt << std::endl;
-      updateFromInputs(window, dt, &camera);
-		}
-
+		float dt = currentTime - lastTime;
 		lastTime = currentTime;
 
+		updateFromInputs(window, dt, &camera);
+
+		std::cout << "FPS: " << 1.0f / dt << std::endl;
+		
 		renderer.render(camera);
 
     glfwSwapBuffers(window);
