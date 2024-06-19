73bb44a16197661672a85c703d408753e0d467d0
Juanxpeke <juanintendo.f11@gmail.com>
2024-06-18 23:56:08 -0400

    feat added abstract buffer support to gpu mesh

--- source/render/gpu_mesh.h
+++ source/render/gpu_mesh.h
@@ -3,6 +3,7 @@
 #include <cstdint>
 #include <vector>
 #include "../math/primitives.h"
+#include "gpu_buffer.h"
 
 namespace Lotus
 {
@@ -12,13 +13,16 @@ namespace Lotus
     GPUMesh(const std::vector<Lotus::Vertex>& vertices, const std::vector<unsigned int>& indices);
     ~GPUMesh();
 
+    GPUMesh& operator=(const GPUMesh& other) = delete;
+
     uint32_t getVertexArrayID() const { return vertexArrayID; }
     uint32_t getIndicesCount() const { return indicesCount; }
 
   private:
     uint32_t vertexArrayID;
-    uint32_t vertexBufferID;
-    uint32_t indexBufferID;
+    VertexBuffer vertexBuffer;
+    IndexBuffer indexBuffer;
+
     uint32_t indicesCount;
   };
 }
